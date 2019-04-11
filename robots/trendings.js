c13d5aee99f3b26d206bf1bc78b5d388df206b8a
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2019-04-10 21:00:39 -0300

    Partial implementation of robot of get trending topics

--- robots/trendings.js
+++ robots/trendings.js
@@ -1,5 +1,27 @@
+const Twitter = require('twitter');
+
+const twitterCredentials = require('../credentials/twitter.json')
+
+const WOEID_ID = '30' // Brasil
+
+const client = new Twitter({
+  consumer_key: twitterCredentials.consumer_key,
+  consumer_secret: twitterCredentials.consumer_secret,
+  access_token_key: twitterCredentials.access_token_key,
+  access_token_secret: twitterCredentials.access_token_secret
+});
+
+const params = {screen_name: 'nodejs'}
+
 async function robot() {
 
+	async function getTrendingTopicsTwitter() {
+		client.get(`trends/place.json?id=${WOEID_ID}`, params, (error, response) => {
+			if(!error) {
+				console.log(response)
+			}
+		})
+	}
 
 }
 
