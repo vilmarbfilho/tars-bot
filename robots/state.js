ee44e45424c89e55573d72208fc20b9dae07dee4
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2019-04-13 12:12:55 -0300

    Adjustment on name of file

--- robots/state.js
+++ robots/state.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 
-function save(nameFile, data) {
+function save(data) {
 	const dataString = JSON.stringify(data)
-	return fs.writeFileSync(`./content/${nameFile}.json`, dataString)
+	return fs.writeFileSync('./content/content.json', dataString)
 }
 
-function load(nameFile) {
-	const fileBuffer = fs.readFileSync(`./content/${nameFile}.json`, 'utf-8')
+function load() {
+	const fileBuffer = fs.readFileSync('./content/content.json', 'utf-8')
 	return JSON.parse(fileBuffer)
 }
 
