c30aec1de7d2bfb398a1b512dc9890a45df8dfc6
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2019-04-16 14:57:19 -0300

    Implementation robot of sentiments

---
+++ robots/sentiments.js
@@ -0,0 +1,54 @@
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1.js')
+
+const state = require('./state.js')
+
+const nlu = new NaturalLanguageUnderstandingV1({
+    iam_apikey: watsonApiKey,
+    version: '2018-04-05',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+ })
+
+async function robot() {
+    const content = state.load()
+
+    await analyzeSentimentsOfAllTreads(content)
+
+    state.save(content)
+
+    console.log('>> Analyze of sentiments finish and saved')
+
+    async function analyzeSentimentsOfAllTreads(trends) {
+        for (const trend of trends) {
+            console.log(`>>> Analyze sentiments of: ${trend.name}`)
+            await analyzeSentimentsOfAllTweets(trend.tweets)
+        }
+    }
+
+    async function analyzeSentimentsOfAllTweets(tweets) {
+        for (const tweet of tweets) {
+            tweet.sentiment = await analyzeSentence(tweet.text)
+        }
+    }
+
+    async function analyzeSentence(sentence) {
+        return new Promise((resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    sentiment: {}
+                }
+            }, (error, response) => {
+                if (error) {
+                    throw error
+                }
+
+                console.log(response.sentiment.document)
+
+                resolve(response.sentiment.document)
+            })
+        })
+    }
+}
+
+module.exports = robot
